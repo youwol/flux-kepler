e4864d316fd9abad3f915cb5c1dd047140a55c31
greinisch <greinisch@youwol.com>
2021-07-13 09:19:14 +0200

    :zap: include auto-generated

---
+++ src/auto_generated.ts
@@ -0,0 +1,6 @@
+export let AUTO_GENERATED = {
+    name: "@youwol/flux-kepler",
+    namespace: "youwol",
+    version: "0.0.4",
+    description: "Collection of modules to work with structured 3D data"
+}
