5d8f14310cab75405070d5b87af8c01841d29469
greinisch <greinisch@youwol.com>
2021-11-15 16:58:51 +0100

    :bookmark: start 0.0.7-next

--- src/auto_generated.ts
+++ src/auto_generated.ts
@@ -1,6 +1,6 @@
 export let AUTO_GENERATED = {
     name: "@youwol/flux-kepler",
     namespace: "youwol",
-    version: "0.0.7",
+    version: "0.0.7-next",
     description: "Collection of modules to work with structured 3D data"
 }
